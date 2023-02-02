2c45c82cbef2459a5af14a9ae410a8c89089ceaa
Ahmar <ahmarzaidi07@gmail.com>
2023-02-02 22:51:50 +0530

    Add Update And Delete Functionality

--- controllers/postsController.go
+++ controllers/postsController.go
@@ -9,6 +9,7 @@ import (
 // CREATE
 func PostsCreate(c *gin.Context) {
 
+	// Get data of req body
 	var body struct {
 		Body  string
 		Title string
@@ -55,6 +56,7 @@ func PostsRead(c *gin.Context) {
 
 	id := c.Param("id")
 
+	// Find the post to update
 	var post models.Post
 	result := initializers.DB.First(&post, id) // SELECT * FROM users WHERE id = 10;
 
@@ -68,4 +70,58 @@ func PostsRead(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"post": post,
 	})
+}
+
+// Update
+func PostsUpdate(c *gin.Context) {
+	// Get id from the url
+	id := c.Param("id")
+
+	// Get data of req body
+	var body struct {
+		Body  string
+		Title string
+	}
+
+	c.Bind(&body)
+
+	// Find the post to update
+	var post models.Post
+	result := initializers.DB.First(&post, id) // SELECT * FROM users WHERE id = 10;
+
+	// Error
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
+
+	// Update
+	// Update attributes with `struct`, will only update non-zero fields
+	initializers.DB.Model(&post).Updates(models.Post{
+		Title: body.Title,
+		Body:  body.Body,
+	}) // UPDATE users SET name='hello', age=18, updated_at = '2013-11-17 21:34:10' WHERE id = 111;
+
+	// Respond with it
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+// DELETE
+func PostsDelete(c *gin.Context) {
+	// Get id from URL
+	id := c.Param("id")
+
+	// Delete Post
+	result := initializers.DB.Delete(&models.Post{}, id) // DELETE FROM users WHERE id = 10;
+
+	// Error
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
+
+	// Respond
+	c.Status(200)
 }
--- main.go
+++ main.go
@@ -14,10 +14,19 @@ func init() {
 func main() {
 
 	r := gin.Default()
+
+	// CREATE
 	r.POST("/posts", controllers.PostsCreate)
+
+	// READ
 	r.GET("/posts", controllers.PostsReadAll)
 	r.GET("/posts/:id", controllers.PostsRead)
+
+	// UPDATE
+	r.PUT("/posts/:id", controllers.PostsUpdate)
+
+	// DELETE
+	r.DELETE("/posts/:id", controllers.PostsDelete)
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
-
-//
